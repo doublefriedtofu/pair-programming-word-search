1b4ec4a421716206cdcf43178802e78099c76455
doublefriedtofu <hurdonghyuk@gmail.com>
2022-10-26 21:55:24 -0400

    fixed test Max and Russell

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const emptyArr = [];
 
-module.exports = wordSearch
+  for (let i = 0; i < letters[0].length; i++) {
+    let temp = [];
+    for (let j = 0; j < letters.length; j++) {
+      temp.push(letters[j][i]);
+    }
+    emptyArr.push(temp);
+  }
+
+  // filter through horizontal array
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  // filter through vertical array
+  const verticalJoin = emptyArr.map(ls => ls.join(''));
+  for (const m of verticalJoin) {
+    if (m.includes(word)) return true;
+  }
+  return false;
+};
+module.exports = wordSearch;
